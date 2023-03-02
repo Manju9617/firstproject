04ad1adaffda986386f06ea2fa44e7df47be05fc
Manju9617 <dmanjunath419@gmail.com>
2023-03-02 22:16:17 +0530

    02 march

---
+++ src/function based component/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+class Nav extends Component
+{
+    render()
+{
+    return <h1> class nav </h1>
+}
+}
+export default Nav
---
+++ src/function based component/Switch.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+class Switch extends Component
+{
+    render()
+{
+    return <h1> class Switch </h1>
+}
+}
+export default Switch
