03c13ae562f47605ac4b7eabdf39435d79006d64
Manju9617 <dmanjunath419@gmail.com>
2023-03-11 11:45:15 +0530

    props drilling

---
+++ src/App7.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Parent from './Parent';
+
+const App7 = () => {
+  return (
+    <div>
+        <Parent data={100}/>
+    </div>
+  )
+}
+
+export default App7
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
 import React from 'react'
 import {createRoot} from "react-dom/client"
-import App5 from './App5';
-createRoot(document.getElementById('root')).render(<App5/>)
+import App7 from './App7';
+createRoot(document.getElementById('root')).render(<App7/>)
