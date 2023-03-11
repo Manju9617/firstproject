63341ab0cf3658728ade832abaaddad6296e53cf
Manju9617 <dmanjunath419@gmail.com>
2023-03-11 12:04:16 +0530

    boolen props

---
+++ src/App6.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Course from './Course';
+
+const App6 = () => {
+  return (
+    <>
+    <Course courseDetails={{
+        courseName:"JAVA FULL STACK",
+        courseAvail:true,
+        courseDuration: "3 months"}}/>
+    </>
+  )
+}
+
+export default App6
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-
 import React from 'react'
 import {createRoot} from "react-dom/client"
-import App7 from './App7';
-createRoot(document.getElementById('root')).render(<App7/>)
+
+import App6 from './App6';
+createRoot(document.getElementById('root')).render(<App6/>)
